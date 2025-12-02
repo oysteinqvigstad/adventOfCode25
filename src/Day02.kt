3af4c45cc041ecc5eb66f11f13def395d5e9da2d
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2025-12-02 13:33:02 +0100

    simplified a bit

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,12 +2,12 @@
 class Day02(input: List<String>) {
     private val data = input[0].split(',').map { parse(it) }
 
-    val answer1 = data.flatMap { range -> range.filter { number -> hasRepeatingPattern(number.toString(), false) } }.sum()
-    val answer2 = data.flatMap { range -> range.filter { number -> hasRepeatingPattern(number.toString(), true) } }.sum()
+    val answer1 = data.flatMap { range -> range.filter { number -> hasRepeatingPattern(number, false) } }.sum()
+    val answer2 = data.flatMap { range -> range.filter { number -> hasRepeatingPattern(number, true) } }.sum()
 
     fun parse(input: String): LongRange =
         input.split('-').let { (start, end) -> start.toLong()..end.toLong() }
 
-    fun hasRepeatingPattern(s: String, multipleRepeat: Boolean): Boolean =
-        Regex("^(.+)\\1${if (multipleRepeat) "+" else ""}$").matches(s)
+    fun hasRepeatingPattern(n: Long, multipleRepeat: Boolean): Boolean =
+        Regex("^(.+)\\1${if (multipleRepeat) "+" else ""}$").matches(n.toString())
 }
