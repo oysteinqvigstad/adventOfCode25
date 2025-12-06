6acbf63ab7c570d329691192a3b51c62492663f1
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2025-12-06 13:09:02 +0100

    changed variable names to align better with puzzle semantics

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,26 +1,23 @@
 fun String.toRange(): LongRange = split('-').let { (a, b) -> a.toLong()..b.toLong() }
 
 class Day05(input: List<String>) {
-    val freshRanges = mergeOverlapping(input.takeWhile(String::isNotEmpty).map(String::toRange))
+    val freshRanges = input.takeWhile(String::isNotEmpty).map(String::toRange).run(::mergeOverlap)
     val inventory = input.mapNotNull(String::toLongOrNull)
 
     val answer1 = inventory.count { id -> freshRanges.any { range -> id in range } }
     val answer2 = freshRanges.sumOf { range -> range.last - range.first + 1L }
 
-    fun mergeOverlapping(ranges: List<LongRange>): List<LongRange> {
+    fun mergeOverlap(ranges: List<LongRange>): List<LongRange> {
         val sorted = ranges.sortedBy(LongRange::first)
-        val merged = mutableListOf<LongRange>()
-        merged.add(sorted.first())
-        for (current in sorted.drop(1)) {
-            val last = merged.last()
-            if (current.first > last.last) {
-                merged.add(current)  // no overlap with previous.
+        val merged = mutableListOf(sorted.first())
+        for (curr in sorted.drop(1)) {
+            val prev = merged.last()
+            if (curr.first > prev.last) {
+                merged.add(curr)  // no overlap with previous.
             } else {
-                merged[merged.size-1] = last.first..maxOf(current.last, last.last)
+                merged[merged.size-1] = prev.first..maxOf(curr.last, prev.last)
             }
         }
         return merged
     }
 }
-
-
