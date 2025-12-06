595af33e08c64136d0b126544d74d614f467ccc3
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2025-12-06 13:11:50 +0100

    solved today's puzzles

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,8 +3,7 @@ typealias Op = (Long, Long) -> Long
 class Day06(input: List<String>) {
     val table = input.map(::splitByWhitespace)
     val operators = table.last().map(::makeOperator)
-    val columnWidths = getColumnWidths(table)
-    val values = makeValues(input.dropLast(1), columnWidths)
+    val values = makeValues(input.dropLast(1), getColumnWidths(table))
 
     val answer1 = values.zip(operators).sumOf { (value, op) -> computeHorizontally(value, op) }
     val answer2 = values.zip(operators).sumOf { (value, op) -> computeVertically(value, op) }
@@ -12,7 +11,7 @@ class Day06(input: List<String>) {
     /**
      * Extracts numbers (as strings) while preserving vertical alignment
      */
-    fun makeValues(input: List<String>, widths: List<Int>) =
+    fun makeValues(input: List<String>, widths: List<Int>): List<List<String>> =
         widths.drop(1)
               .runningFold(0..<widths.first()) { prev, value -> prev.last+2..prev.last+1+value }
               .map { r -> input.map { it.substring(r) } }
@@ -35,7 +34,7 @@ class Day06(input: List<String>) {
      * Values have variable length, this computes the size of the entire column
      * by selecting the size of the value with the largest width
      */
-    fun getColumnWidths(table: List<List<String>>) =
+    fun getColumnWidths(table: List<List<String>>): List<Int> =
         (0 until table.first().size)
             .map { col -> (0 until table.size).maxOf { row -> table[row][col].length } }
 
@@ -46,6 +45,6 @@ class Day06(input: List<String>) {
         else -> throw(IllegalArgumentException("illegal operation"))
     }
 
-    fun splitByWhitespace(input: String) =
+    fun splitByWhitespace(input: String): List<String> =
         input.trim().split(Regex("\\s+"))
 }
